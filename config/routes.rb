11662fb9e61ae74d5880c77dd4ad392e955c12ae
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-04-06 18:47:46 +0200

    Feat - Add HP & navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   devise_for :users
   resources :quotes
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "quotes#index"
 end
