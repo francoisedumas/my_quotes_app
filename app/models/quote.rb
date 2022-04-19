d0f3c46ef0cfaa513b7e7f16e329135dee24de91
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-04-19 09:30:58 +0200

    Feat - Ensure companies only see their own updates

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,5 +9,5 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # all 3 above can be replaced by 1 line!
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
